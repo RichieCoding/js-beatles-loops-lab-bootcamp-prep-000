884fea8b53811d269f859884fe3d2bc4c03d329f
Richard Li <richardli125@gmail.com>
2019-04-09 22:36:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  let arr = [];
+  for (var i = 0; i < musicians.length; i ++ ) {
+    arr.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  let newArray = [];
+  let i = 0;
+  while (i < facts.length) {
+    newArray.push(facts[i] + '!!!')
+    i ++;
+  }
+  return newArray;
+}
